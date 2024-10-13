906411e93dcfeb60d66b3cb87b94fd6c50c24368
SummerToday <kom347@naver.com>
2024-10-13 22:37:15 +0900

    자동 로그인 관련 로그인 상태 체크 api 추가

--- src/main/java/com/backend/soonsullengguide/controller/GoogleAuthController.java
+++ src/main/java/com/backend/soonsullengguide/controller/GoogleAuthController.java
@@ -62,6 +62,37 @@ public class GoogleAuthController {
         }
     }
 
+    @PostMapping("/check-login-status")  // GetMapping에서 PostMapping으로 변경
+    public ResponseEntity<Map<String, Object>> checkLoginStatus(@RequestHeader("Authorization") String refreshToken) {
+        Map<String, Object> response = new HashMap<>();
+
+        // Bearer 토큰이 포함된 경우 제거
+        if (refreshToken != null && refreshToken.startsWith("Bearer ")) {
+            refreshToken = refreshToken.substring(7);
+        }
+
+        if (refreshToken != null && jwtTokenProvider.validateToken(refreshToken)) {
+            String email = jwtTokenProvider.getEmailFromToken(refreshToken);
+            Optional<User> userOptional = userRepository.findByEmail(email);
+
+            if (userOptional.isPresent() && userOptional.get().getRefreshToken().equals(refreshToken)) {
+                response.put("isLoggedIn", true);
+                System.out.println("자동 로그인 성공: " + email);  // 로그인 성공 로그 출력
+                return ResponseEntity.status(HttpStatus.OK).body(response);
+            } else {
+                response.put("isLoggedIn", false);
+                System.out.println("로그인 실패: 리프레시 토큰 불일치");  // 리프레시 토큰 불일치 로그 출력
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+            }
+        } else {
+            response.put("isLoggedIn", false);
+            System.out.println("로그인 실패: 토큰 유효성 검사 실패");  // 토큰 유효성 검사 실패 로그 출력
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
+    }
+
+
+
     @PostMapping("/refresh-token")
     public ResponseEntity<Map<String, Object>> refreshAccessToken(@RequestBody Map<String, String> request) {
         String refreshToken = request.get("refreshToken");
